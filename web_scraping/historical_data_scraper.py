e4c86aa6a346cf7a72fe9bc9a00ac6dab79e6e70
NICOLAS-ANTONIO <worst-mop-acutely@duck.com>
2024-08-31 13:45:36 -0400

    Actualización diaria de datos: 2024-08-31 13:45:36

--- web_scraping/historical_data_scraper.py
+++ web_scraping/historical_data_scraper.py
@@ -1,7 +1,8 @@
 import yfinance as yf
-import datetime as datetime
-import os, sys
-sys.path.append(os.getcwd)
+import datetime
+import os
+import sys
+sys.path.append(os.getcwd())
 
 # Verifica si existe una carpeta 'datasets', si no existe la crea
 if not os.path.exists('datasets/'):
@@ -18,7 +19,6 @@ def download_historical_data(ticker, start_date, end_date, output):
     df = df.reset_index()
     
     # Guarda el dataset
-    output
     df.to_csv(output, index=False)
     
 # Diccionario de nombres de empresas con su NYSE
@@ -28,11 +28,16 @@ tickers_dict = {'bbva':'BBVA', 'santander':'SAN', 'jpmorgan':'JPM', 'goldman_sac
                 'cemex':'CX', 'grupo_bimbo':'BIMBOA.MX', 'cocacola_femsa':'KOF'
                 }
 
+# Fecha de inicio fija
 start_date = '2023-01-01'
-end_date = '2024-08-30'
+
+# Fecha de fin se actualiza a la fecha actual
+end_date = datetime.date.today().strftime('%Y-%m-%d')
 
 # Ciclo para iterar sobre todas las empresas
 for key, ticker in tickers_dict.items():
     output_file = f'datasets/{key}_historical_data.csv'
     download_historical_data(ticker, start_date, end_date, output_file)
-    print(f'se ha guardado el dataset de {key}')
+    print(f'Se ha guardado el dataset de {key}')
+
+print(f'Datos actualizados hasta: {end_date}')
