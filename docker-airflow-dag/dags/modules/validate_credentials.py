f82062714f003e077a20d2c5aae8be70d14cff97
Juan Cortez Zamar <106788005+juancorzamar93@users.noreply.github.com>
2024-09-08 16:09:30 -0300

    modificando credenciales y codigos

--- docker-airflow-dag/dags/modules/validate_credentials.py
+++ docker-airflow-dag/dags/modules/validate_credentials.py
@@ -14,7 +14,8 @@ def validate_credentials():
         "POSTGRES_USERNAME": os.getenv('POSTGRES_USERNAME'),
         "POSTGRES_PASSWORD": os.getenv('POSTGRES_PASSWORD'),
         "POSTGRES_DB": os.getenv('POSTGRES_DB'),
-        "POSTGRES_PORT": os.getenv('POSTGRES_PORT')
+        "POSTGRES_PORT": os.getenv('POSTGRES_PORT'),
+        "POSTGRES_HOST":os.getenv('POSTGRES_HOST')
     }
 
     for key, value in credentials.items():
