e675d1923f356050d70bd1ebae40dafe67d04dcc
Juan Cortez Zamar <106788005+juancorzamar93@users.noreply.github.com>
2024-09-08 23:26:31 +0000

    #

--- docker-airflow-dag/dags/modules/load_data.py
+++ docker-airflow-dag/dags/modules/load_data.py
@@ -62,15 +62,61 @@ import os
 #     except Exception as e:
 #         print(f"Error al cargar datos: {e}")
 
-def load_data_to_db(file_path):
-    # Extraer el nombre del archivo sin la extensión
-    table_name = os.path.splitext(os.path.basename(file_path))[0]
+# def load_data_to_db(file_path):
+#     # Extraer el nombre del archivo sin la extensión
+#     table_name = os.path.splitext(os.path.basename(file_path))[0]
 
-    # Leer el archivo CSV en un DataFrame
-    data = pd.read_csv(file_path)
+#     # Leer el archivo CSV en un DataFrame
+#     data = pd.read_csv(file_path)
 
-    # Crear la conexión a Amazon Redshift
+#     # Crear la conexión a Amazon Redshift
+#     try:
+#         conn = psycopg2.connect(
+#             dbname=POSTGRES_DB,
+#             user=POSTGRES_USERNAME,
+#             password=POSTGRES_PASSWORD,
+#             host=POSTGRES_HOST,
+#             port=POSTGRES_PORT
+#         )
+#         cursor = conn.cursor()
+
+#         # Crear la tabla si no existe
+#         create_table_query = f"""
+#         CREATE TABLE IF NOT EXISTS {table_name} (
+#             Date DATE,
+#             Open FLOAT,
+#             High FLOAT,
+#             Low FLOAT,
+#             Close FLOAT,
+#             Volume BIGINT,
+#             Dividends FLOAT,
+#             Stock_Splits FLOAT
+#         );
+#         """
+#         cursor.execute(create_table_query)
+
+#         # Insertar los datos fila por fila
+#         for index, row in data.iterrows():
+#             insert_query = f"""
+#             INSERT INTO {table_name} (Date, Open, High, Low, Close, Volume, Dividends, Stock_Splits)
+#             VALUES ('{row['Date']}', {row['Open']}, {row['High']}, {row['Low']}, {row['Close']}, {row['Volume']}, {row['Dividends']}, {row['Stock Splits']});
+#             """
+#             cursor.execute(insert_query)
+
+#         conn.commit()
+#         cursor.close()
+#         conn.close()
+#         print(f"Datos cargados en la tabla {table_name} correctamente.")
+    
+#     except Exception as e:
+#         print(f"Error al cargar datos: {e}")
+
+def load_data_to_db(file_path, table_name):
     try:
+        # Leer el archivo CSV en un DataFrame
+        data = pd.read_csv(file_path)
+
+        # Conexión a Amazon Redshift
         conn = psycopg2.connect(
             dbname=POSTGRES_DB,
             user=POSTGRES_USERNAME,
@@ -80,37 +126,31 @@ def load_data_to_db(file_path):
         )
         cursor = conn.cursor()
 
-        # Crear la tabla si no existe
-        create_table_query = f"""
-        CREATE TABLE IF NOT EXISTS {table_name} (
-            Date DATE,
-            Open FLOAT,
-            High FLOAT,
-            Low FLOAT,
-            Close FLOAT,
-            Volume BIGINT,
-            Dividends FLOAT,
-            Stock_Splits FLOAT
-        );
-        """
-        cursor.execute(create_table_query)
+        # Cargar los datos en la tabla correspondiente
+        for i, row in data.iterrows():
+            columns = list(row.index)  # Nombres de las columnas del DataFrame
+            values = [row[col] for col in columns]  # Valores de cada fila
 
-        # Insertar los datos fila por fila
-        for index, row in data.iterrows():
-            insert_query = f"""
-            INSERT INTO {table_name} (Date, Open, High, Low, Close, Volume, Dividends, Stock_Splits)
-            VALUES ('{row['Date']}', {row['Open']}, {row['High']}, {row['Low']}, {row['Close']}, {row['Volume']}, {row['Dividends']}, {row['Stock Splits']});
-            """
-            cursor.execute(insert_query)
+            # Crear la consulta SQL para insertar los datos
+            insert = sql.SQL("INSERT INTO {} ({}) VALUES ({})").format(
+                sql.Identifier(table_name),
+                sql.SQL(', ').join(map(sql.Identifier, columns)),
+                sql.SQL(', ').join(sql.Placeholder() * len(values))
+            )
 
+            # Ejecutar la consulta SQL con los valores de la fila
+            cursor.execute(insert, values)
+
+        # Confirmar los cambios en la base de datos
         conn.commit()
         cursor.close()
         conn.close()
         print(f"Datos cargados en la tabla {table_name} correctamente.")
-    
-    except Exception as e:
-        print(f"Error al cargar datos: {e}")
 
+    except psycopg2.Error as e:
+        print(f"Error al cargar los datos en la base de datos: {e}")
+    except Exception as e:
+        print(f"Error general: {e}")
 
 def load_all_data_from_directory(directory):
     dags_folder = os.path.dirname(os.path.abspath(__file__))
