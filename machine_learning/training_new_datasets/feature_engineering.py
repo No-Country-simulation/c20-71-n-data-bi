f4980986bebb460f0483e6a69f7706125ca55f4b
juan antonio <juan_ryes.94@hotmail.com>
2024-09-15 12:37:25 -0600

    se agrega rolling_mean

--- machine_learning/training_new_datasets/feature_engineering.py
+++ machine_learning/training_new_datasets/feature_engineering.py
@@ -3,14 +3,21 @@ def engineer_features(df):
     # shift la variable objetivo
     df['Next_Day_Return'] = df['Daily_Return'].shift(-1)
     
-    # Lag features: valores pasados de una variable
-    lag_features = ['Close', 'EMA20', 'SMA50', 'RSI', 'OBV', 'MACD', 'Stoch_D', 'Stoch_K', 'ROC', 'Volume_SMA20']
+    # Lag features (Características de desfase): valores pasados de una variable
+    lag_features = ['Close', 'EMA20', 'SMA50', 'OBV', 'Stoch_D', 'Stoch_K', 'ROC', 'Volume_SMA20']
     for feature in lag_features:
         df[f'{feature}_Lag1'] = df[feature].shift(1)
         df[f'{feature}_Lag2'] = df[feature].shift(2)
-        
+    
+    # Media movil
+    for feature in lag_features:
+        df[f'{feature}_rolling_mean'] = df[feature].shift().rolling(10).mean()
+    
     # Agrega el dia de la semana
     df['Day_Of_week'] = df.index.dayofweek
+    
+    # Se borran columnas más irrelevantes para el modelo
+    df = df.drop(['Year', 'Stoch_D', 'RSI', 'Day', 'Month', 'Stoch_K'], axis=1)
     
     # Borra los valores nulos que se crearon
     df = df.dropna()
