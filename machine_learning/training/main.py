8e6616d26d6e7c5350109eca6f806c22d0fa5068
juan antonio <juan_ryes.94@hotmail.com>
2024-09-18 18:59:06 -0600

    codigo para guardar predicciones

--- machine_learning/training/main.py
+++ machine_learning/training/main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import os, sys
 sys.path.append(os.getcwd)
-#import joblib
 from connection import connect_to_redshift
 from data_extraction import fetch_data_from_redshift
 from config import REDSHIFT_CONFIG
@@ -9,7 +8,7 @@ from preprocessing import preprocess_data
 from engineer_feature import fetures_engineering
 from model import train_model, evaluate_model, make_prediction
 
-def main(company_name):
+def model(company_name):
     
     connection = connect_to_redshift(REDSHIFT_CONFIG)
     if not connection:
@@ -56,12 +55,46 @@ def main(company_name):
     prediction = make_prediction(model, scaler, latest_data)
     
     print(f"Prediction for next day: {'Up' if prediction[0] == 1 else 'Down'}")
-
-if __name__ == "__main__":
     
-    company_name = 'ypf'
-    result = main(company_name)
-    if result is None:
+    prediction = 'Up' if prediction[0] == 1 else 'Down'
+    
+    description = 'Probabilidad de que suba' if prediction[0] == 1 else 'Probabilidad de que baje'
+    
+    return prediction, description
+
+
+comercial_name = ['YPF Sociedad Anonima (YPF)',
+                  'Pampa Energía S.A. (PAM)',
+                  'Ecopetrol S.A. (EC)',
+                  'Petróleo Brasileiro S.A. - Petrobras (PBR)',
+                  'Interconexión Eléctrica S.A. E.S.P. (IESFY)',
+                  'Centrais Elétricas Brasileiras S.A. - Eletrobrás (EBR)',
+                  'Engie Brasil Energia S.A. (EGIE3.SA)',
+                  'Vista Energy, S.A.B. de C.V. (VIST)',
+                  'Atlas Energy Solutions Inc. (AESI)',
+                  'Compañía de Minas Buenaventura S.A.A. (BVN)'
+                  ]
+
+company_name = ['ypf', 'pampa_energía', 'ecopetrol', 'petrobras', 'interconexion_electrica', 'eletrobras', 'engie_brasil_energia',
+'vista_energy', 'eas_andes', 'minas_buenaventura']
+
+predictions = []
+descriptions = []
+
+for company in company_name:
+    prediction, description = model(company)
+    if prediction is None:
         sys.exit(1)
     
-#joblib.dump(model_rfc, f'machine_learning/model/model_rfc_{company_name}.joblib')
+    predictions.append(prediction)
+    descriptions.append(description) 
+
+my_dict = {'Nombre de la empresa':comercial_name,
+           'Predicción':predictions,
+           'Descripción':description
+           }
+
+print(len(predictions))
+print(len(descriptions))
+my_df = pd.DataFrame(my_dict)
+my_df.to_csv('predicciones/predicciones.csv')
